5c70e04d0538d39b7db8dd78cb61e6a923044829
jungyurii <ikakete@gmail.com>
2024-08-07 00:09:27 +0900

    first commit

---
+++ src/main/java/com/spring/board_summer/BoardSummerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.board_summer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardSummerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardSummerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/board_summer/BoardSummerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.board_summer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardSummerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
